9b2516049a60c240151389996510836ad211f698
thesuz88 <stevensuzio@gmail.com>
2017-06-26 17:50:50 -0400

    initial commit

---
+++ src/com/jetbrains/Main.java
@@ -0,0 +1,56 @@
+package com.jetbrains;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        double lengthOfRoom = 0;
+        double widthOfRoom = 0;
+        double heightOfRoom = 0;
+
+            System.out.println("Welcome to Grand Circus' Room Detail Generator!");
+            System.out.println(" ");
+
+        do {
+            //User entered length of room
+            System.out.println("Enter length of room: ");
+
+            while (!scnr.nextDouble()) {
+                System.out.println("Please only enter a number.");
+                System.out.println("Enter length of room: ");
+                lengthOfRoom = scnr.nextDouble();
+            }
+        }
+        while (true) {
+
+                //User entered width of room
+            System.out.println("Enter width of room: ");
+                widthOfRoom = scnr.nextDouble();
+
+            System.out.println("Enter height of room: ");
+            heightOfRoom = scnr.nextDouble();
+
+                //Takes user input and calculates area and perimeter of room
+            double areaOfRoom = lengthOfRoom * widthOfRoom;
+            double perimOfRoom = 2 * lengthOfRoom + 2 * widthOfRoom;
+            double volOfRoom = lengthOfRoom * widthOfRoom * heightOfRoom;
+
+                //Prints out calculated area, perimeter, and volume and asks user if they would like to continue
+            System.out.println("Area: " + areaOfRoom);
+            System.out.println("Perimeter: " + perimOfRoom);
+            System.out.println("Volume: " + volOfRoom);
+            System.out.println(" ");
+            System.out.println("Continue? (y/n): ");
+
+                //If user enters "y" loop continues, if user enters "n" loop breaks
+            if (scnr.next().equals("n")) {
+                break;
+            }
+
+        }
+
+
+
+    }
+}
